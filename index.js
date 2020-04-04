77942f92fdd3258b36d3a2139a061a8d0bac4132
Alex9523 <siaskyi9523@gmail.com>
2020-04-05 00:32:55 +0300

    deploy v1.0

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
-// var today = new Date().toISOString();
-// convertDateToUTC(date) ;{ 
-//     return new Date(date.getUTCFullYear(), 
-//                     date.getUTCMonth(), 
-//                     date.getUTCDate(), 
-//                     date.getUTCHours(), 
-//                     date.getUTCMinutes(), 
-//                     date.getUTCSeconds()) }
-    // var element = document.querySelector('.actual-time');
-    // element.innerHTML = "today";
+$(document).ready(function(){onLoad()})
+function startTime() {
+    const date=new Date();
+    let time = date.toLocaleString('en', { hour: 'numeric' , minute: 'numeric'});
+    let dayOfWeek = date.toLocaleString('en', { weekday: 'long' });
+    let numberOfWeek = date.getDate();
+    let month = date.toLocaleString('en', { month: 'long' } );
+    let year = date.toLocaleString('en', {year: 'numeric'})
+    $('#txt').html(`<h3>Current UTC time: ${time}</h3> <p>${dayOfWeek}, ${numberOfWeek} ${month} ${year}</p> `);
+    
+}
 
 
-let howManyAstronauts =0;
-const requestURLForIss = "http://api.open-notify.org/iss-now.json"
+
 const requestURLForAstros ="http://api.open-notify.org/astros.json"
+const requestURLForIss = "http://api.open-notify.org/iss-now.json"
 const xhr = new XMLHttpRequest();
 const xhr1 = new XMLHttpRequest();
 
 
+function show(){
 
 xhr.open('GET', requestURLForIss, true);
 xhr.send(null);
 xhr.onload = function () {
+    
     if (xhr.readyState === xhr.DONE) {
         if (xhr.status === 200) {
             const data = xhr.responseText
             const jsonResponse = JSON.parse(data);
-            console.log(jsonResponse["iss_position"]);
-            var element = document.querySelector('.actual-located');
-            element.innerHTML = `Latitude: ${jsonResponse["iss_position"]["latitude"]}
-                                 Longitude: ${jsonResponse["iss_position"]["longitude"]}`;
+            var a = Number(jsonResponse["iss_position"]["latitude"]);
+            var b = Number(jsonResponse["iss_position"]["longitude"]);
+            console.log(a, typeof(a),b, typeof(b))
+            const element = $('.actual-located');
+            element.html(`Latitude: ${a} Longitude: ${b}`);
+                                 
         }
     }
+    initMap(a,b)
 };
+}
+
+function onLoad(){
+    show();  
+    setInterval('show()',5000);  
+    startTime()
+    setInterval('startTime()',5000);
+}
 
 
-function initMap() {
-    var myLatLng = {lat: 28.1744 , lng: 43.4108};
+
+function initMap(lat, lng) {
+    var myLatLng = {lat: lat , lng: lng};
 
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 4,
@@ -49,6 +64,8 @@ function initMap() {
     });
   }
 
+
+
 xhr1.open('GET', requestURLForAstros, true);
 xhr1.send(null);
 xhr1.onload = function () {
@@ -57,27 +74,12 @@ xhr1.onload = function () {
             const data = xhr1.responseText
             const jsonResponse = JSON.parse(data);
             
-            const astronauts= document.querySelector(".astronauts");
+            const astronauts= $(".astronauts");
             for(let i =0; i< jsonResponse["people"].length;i++){
-                const str = `<div class="astronaut-name"><p>${jsonResponse["people"][i]["name"]}</p></div>`;
-                astronauts.innerHTML += str;
+                const str = `<div class="astronaut-name"><p><i class="material-icons middle">account_circle</i>${jsonResponse["people"][i]["name"]}</p></div>`;
+                astronauts.append(str)
             }
-            astronauts.innerHTML += jsonResponse["people"].length;
+            astronauts.append(`<p>Total amount: <b>${jsonResponse["number"]}</b> people on ISS</p>`);
         }
     }
 };
-// xhr.abort();
-
-// // var today = new Date();
-// // today= today.getTime();
-// // console.log(today)
-
-
-
-// //   var xhr = new XMLHttpRequest();
-// //   xhr.open('GET', requestURL);
-// //   xhr.responseType = 'json';
-// //   xhr.send();
-// //   xhr.onload = function() {
-// //   console.log( xhr.responseText);
-// //   }
